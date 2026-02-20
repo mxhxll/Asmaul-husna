2c0a5bb476812716366712e9cd9677c557b99e9b
mxhxll <119546786+mxhxll@users.noreply.github.com>
2026-02-20 16:35:46 +0100

    Add array of names with meanings and transliterations

--- names.js
+++ names.js
@@ -1,1 +1,32 @@
-const NAMES = [];
+const NAMES = [
+{
+id:1,
+arabic:"ٱلرَّحْمَـٰنُ",
+translit:"Ar-Rahman",
+meaning:"Le Tout Miséricordieux"
+},
+{
+id:2,
+arabic:"ٱلرَّحِيمُ",
+translit:"Ar-Rahim",
+meaning:"Le Très Miséricordieux"
+},
+{
+id:3,
+arabic:"ٱلْمَلِكُ",
+translit:"Al-Malik",
+meaning:"Le Souverain"
+},
+{
+id:4,
+arabic:"ٱلْقُدُّوسُ",
+translit:"Al-Quddus",
+meaning:"Le Très Saint"
+},
+{
+id:5,
+arabic:"ٱلسَّلَامُ",
+translit:"As-Salam",
+meaning:"La Paix"
+}
+];
